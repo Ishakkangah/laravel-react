dbb688679327ef0544c53debede146f75515bc25
Ishakkangah <ishakkangah@gmail.com>
2023-04-22 17:40:08 +0700

    install heroicons

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
+import { BeakerIcon } from "@heroicons/react/24/solid";
 
 function App() {
     return (
         <>
-            <div className="bg-red-400">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos,
-                ratione.
+            <div className="bg-red-400 text-center flex justify-center">
+                <button className="bg-blue-400 flex px-10 py-2">
+                    <BeakerIcon className="h-6 w-6 text-blue-500" />
+                    Tombol
+                </button>
             </div>
         </>
     );
