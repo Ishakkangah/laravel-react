0326b31a07c1af6a90b214c3bd23c92797f80ffc
Ishakkangah <ishakkangah@gmail.com>
2023-04-22 17:21:34 +0700

    install react dan tailwindcss

---
+++ react/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div className="bg-red-400">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos,
+                ratione.
+            </div>
+        </>
+    );
+}
+
+export default App;
