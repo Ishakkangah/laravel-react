f80142ce91dd072afaee055ba92f451d3de676e4
Ishakkangah <ishakkangah@gmail.com>
2023-04-23 15:26:25 +0700

    membuat contect provider 1

--- react/src/views/Dashboard.jsx
+++ react/src/views/Dashboard.jsx
@@ -1,30 +1,20 @@
+import PageComponent from "../components/PageComponent";
+
 export default function Dashboard() {
     return (
-        <div>
-            <header className="bg-white shadow">
-                <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                    <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-                        Dashboard
-                    </h1>
-                </div>
-            </header>
-            <main>
-                <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Totam rem ipsam tempora officiis sed quod aliquam minima.
-                    Minima tenetur recusandae sapiente qui amet eligendi aliquid
-                    illum distinctio laborum libero assumenda, nihil repudiandae
-                    ipsa nemo quibusdam, voluptatibus impedit asperiores labore
-                    debitis aspernatur quae. Delectus, saepe officia aut
-                    blanditiis odit, eius est repellat necessitatibus sequi
-                    fugiat, ratione reprehenderit mollitia impedit rem. Dolorum
-                    quasi velit esse. Illo impedit totam sint tenetur corrupti
-                    doloribus excepturi natus doloremque pariatur, voluptatum
-                    possimus explicabo numquam. Harum vitae est eius nulla qui
-                    ea fuga totam a, molestiae reiciendis optio vero unde ad hic
-                    id quo mollitia. Laboriosam, nam.
-                </div>
-            </main>
-        </div>
+        <PageComponent title="Dashboard">
+            Dashboard , Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Maiores nostrum officiis, quaerat nesciunt adipisci consequuntur
+            nisi iste id eaque, aut blanditiis. Fugit quos voluptatibus omnis
+            veniam natus facilis sunt eius laboriosam modi nemo consectetur
+            quisquam quia delectus, soluta alias non nisi numquam molestias
+            architecto. Assumenda reiciendis temporibus quia in eos voluptates.
+            Ipsam perferendis et adipisci nihil dicta expedita earum aliquid est
+            minima, eveniet maiores illum dolorem quaerat. Maxime ducimus
+            facilis, sed perspiciatis placeat natus accusantium possimus
+            sapiente numquam non voluptatem ab at. Esse reprehenderit, aperiam
+            suscipit ipsa labore dolorem, quidem obcaecati recusandae tempora
+            quos consequuntur quis animi, eaque odit velit.
+        </PageComponent>
     );
 }
--- react/src/views/Surveys.jsx
+++ react/src/views/Surveys.jsx
@@ -1,30 +1,5 @@
+import PageComponent from "../components/PageComponent";
+
 export default function Surveys() {
-    return (
-        <div>
-            <header className="bg-white shadow">
-                <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                    <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-                        Surveys
-                    </h1>
-                </div>
-            </header>
-            <main>
-                <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Totam rem ipsam tempora officiis sed quod aliquam minima.
-                    Minima tenetur recusandae sapiente qui amet eligendi aliquid
-                    illum distinctio laborum libero assumenda, nihil repudiandae
-                    ipsa nemo quibusdam, voluptatibus impedit asperiores labore
-                    debitis aspernatur quae. Delectus, saepe officia aut
-                    blanditiis odit, eius est repellat necessitatibus sequi
-                    fugiat, ratione reprehenderit mollitia impedit rem. Dolorum
-                    quasi velit esse. Illo impedit totam sint tenetur corrupti
-                    doloribus excepturi natus doloremque pariatur, voluptatum
-                    possimus explicabo numquam. Harum vitae est eius nulla qui
-                    ea fuga totam a, molestiae reiciendis optio vero unde ad hic
-                    id quo mollitia. Laboriosam, nam.
-                </div>
-            </main>
-        </div>
-    );
+    return <PageComponent title="Surveys">Surveys content</PageComponent>;
 }
