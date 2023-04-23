c0c2c2980a0a221c47795f63710b8d73eabbc443
Ishakkangah <ishakkangah@gmail.com>
2023-04-23 08:22:57 +0700

    membuat guest layout 2

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import GuestLayouts from "./components/GuestLayouts";
 import Dashboard from "./views/Dashboard";
 import Login from "./views/Login";
 import Sign_up from "./views/Sign_up";
@@ -7,19 +8,25 @@ import Surveys from "./views/Surveys";
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Login />,
-    },
-    {
-        path: "/dashboard",
         element: <Dashboard />,
-    },
-    {
-        path: "/sign-up",
-        element: <Sign_up />,
     },
     {
         path: "/surveys",
         element: <Surveys />,
+    },
+    {
+        path: "/",
+        element: <GuestLayouts />,
+        children: [
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/sign-up",
+                element: <Sign_up />,
+            },
+        ],
     },
 ]);
 
