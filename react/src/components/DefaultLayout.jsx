aa8b06900e93a803e9c71a86fde8386b35153132
Ishakkangah <ishakkangah@gmail.com>
2023-04-24 11:11:06 +0700

    membuat contect provider 2

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,25 +1,21 @@
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, UserIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Outlet, NavLink } from "react-router-dom";
+import { userStateContext } from "../contexts/ContextProvider";
 
-const user = {
-    name: "Tom Cook",
-    email: "tom@example.com",
-    imageUrl:
-        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
 const navigation = [
     { name: "Dashboard", to: "/" },
     { name: "Surveys", to: "/surveys" },
 ];
-const userNavigation = [{ name: "Sign_out", href: "#" }];
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(" ");
 }
 
 export default function DefautlLayout() {
+    const { currentUser } = userStateContext();
+
     const logout = (e) => {
         e.preventDefault();
         alert("logout");
@@ -77,11 +73,7 @@ export default function DefautlLayout() {
                                                         <span className="sr-only">
                                                             Open user menu
                                                         </span>
-                                                        <img
-                                                            className="h-8 w-8 rounded-full"
-                                                            src={user.imageUrl}
-                                                            alt=""
-                                                        />
+                                                        <UserIcon className="w-8 h-8 text-white bg-black/25 p-2 rounded-full"></UserIcon>
                                                     </Menu.Button>
                                                 </div>
                                                 <Transition
@@ -155,33 +147,25 @@ export default function DefautlLayout() {
                                 <div className="border-t border-gray-700 pb-3 pt-4">
                                     <div className="flex items-center px-5">
                                         <div className="flex-shrink-0">
-                                            <img
-                                                className="h-10 w-10 rounded-full"
-                                                src={user.imageUrl}
-                                                alt=""
-                                            />
+                                            <UserIcon></UserIcon>
                                         </div>
                                         <div className="ml-3">
                                             <div className="text-base font-medium leading-none text-white">
-                                                {user.name}
+                                                {currentUser.name}
                                             </div>
                                             <div className="text-sm font-medium leading-none text-gray-400">
-                                                {user.email}
+                                                {currentUser.email}
                                             </div>
                                         </div>
                                     </div>
                                     <div className="mt-3 space-y-1 px-2">
-                                        {userNavigation.map((item) => (
-                                            <Disclosure.Button
-                                                key={item.name}
-                                                as="a"
-                                                href="#"
-                                                onClick={(e) => logout(e)}
-                                                className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                                            >
-                                                {item.name}
-                                            </Disclosure.Button>
-                                        ))}
+                                        <Disclosure.Button
+                                            href="#"
+                                            onClick={(e) => logout(e)}
+                                            className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                                        >
+                                            Sign out
+                                        </Disclosure.Button>
                                     </div>
                                 </div>
                             </Disclosure.Panel>
--- react/src/main.jsx
+++ react/src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App.jsx";
 import { RouterProvider } from "react-router-dom";
+import { ContextProvider } from "./contexts/ContextProvider";
 import "./index.css";
 import router from "./router.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <ContextProvider>
+            <RouterProvider router={router} />
+        </ContextProvider>
     </React.StrictMode>
 );
